b4117e9edc293eb2ff96bb8d9cbc5c8531960c03
Tamar-Langenburg <74243323+Tamar-Langenburg@users.noreply.github.com>
2020-12-10 13:11:55 +0100

    Add files via upload

---
+++ fd-map 2/script/script.js
@@ -0,0 +1,13 @@
+function ratestar() {
+  var a;
+  a = document.getElementById("div1");
+  a.innerHTML = "&#xf006;";
+  setTimeout(function () {
+      a.innerHTML = "&#xf123;";
+    }, 1000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf005;";
+    }, 2000);
+}
+ratestar();
+setInterval(ratestar, 3000);
